fd353708a448183679331bef0a4614e169e6661a
JT-TEGA <oghenetega.ukpe@pau.edu.ng>
2025-02-01 23:32:09 +0100

    CSC308

---
+++ Tegacsc308/os_with_bootloader-main/kernel_with_bootloader/target/x86_64-unknown-none/debug/deps/volatile-3555d236b3621080.d
@@ -0,0 +1,8 @@
+/Users/timmyilesanmi/Documents/csc_308/os_with_bootloader/kernel_with_bootloader/target/x86_64-unknown-none/debug/deps/libvolatile-3555d236b3621080.rmeta: /Users/timmyilesanmi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/volatile-0.4.6/src/lib.rs /Users/timmyilesanmi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/volatile-0.4.6/src/access.rs
+
+/Users/timmyilesanmi/Documents/csc_308/os_with_bootloader/kernel_with_bootloader/target/x86_64-unknown-none/debug/deps/libvolatile-3555d236b3621080.rlib: /Users/timmyilesanmi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/volatile-0.4.6/src/lib.rs /Users/timmyilesanmi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/volatile-0.4.6/src/access.rs
+
+/Users/timmyilesanmi/Documents/csc_308/os_with_bootloader/kernel_with_bootloader/target/x86_64-unknown-none/debug/deps/volatile-3555d236b3621080.d: /Users/timmyilesanmi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/volatile-0.4.6/src/lib.rs /Users/timmyilesanmi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/volatile-0.4.6/src/access.rs
+
+/Users/timmyilesanmi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/volatile-0.4.6/src/lib.rs:
+/Users/timmyilesanmi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/volatile-0.4.6/src/access.rs:
---
+++ Tegacsc308/os_with_bootloader-main/kernel_with_bootloader/target/x86_64-unknown-none/debug/kernel_with_bootloader.d
@@ -0,0 +1,1 @@
+/Users/timmyilesanmi/Documents/csc_308/os_with_bootloader/kernel_with_bootloader/target/x86_64-unknown-none/debug/kernel_with_bootloader: /Users/timmyilesanmi/Documents/csc_308/os_with_bootloader/kernel_with_bootloader/src/main.rs
